29cd535b4c63df9f89fc7b9214620fa8a2c5b04d
fengyenchia <yanjia20050504@gmail.com>
2026-09-06 05:40:18 +0800

    chore(live): remove unused link import

--- frontend/app/meetings/[id]/live/page.tsx
+++ frontend/app/meetings/[id]/live/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { IconRefresh, IconWifi } from "@tabler/icons-react";
-import Link from "next/link";
 import { useParams } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
 import { AppShell } from "../../../../components/app-shell";
